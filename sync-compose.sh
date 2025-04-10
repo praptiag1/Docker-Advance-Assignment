d345946562df9a1fbed783bc72c3ac5392b45a65
Prapti Agarwal <prapti.agarwal@nagarro.com>
2025-04-10 12:58:42 +0530

    updated shell script

--- sync-compose.sh
+++ sync-compose.sh
@@ -6,11 +6,8 @@ git pull origin main
 
 # 2. Stop and clean existing containers
 echo "Removing old containers..."
-docker compose down --remove-orphans
-
-# # 3. Pull images (no builds)
-# echo "Pulling latest images..."
-# docker-compose pull
+docker compose down
+docker compose down -v
 
 # 4. Start containers
 echo "Starting services..."
