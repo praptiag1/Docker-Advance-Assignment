95c5218ac558471f3c53eef01227847c54d4a4d6
Prapti Agarwal <prapti.agarwal@nagarro.com>
2025-04-10 12:15:43 +0530

    Added Shell script

---
+++ sync-compose.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 1. Get latest compose file
+echo "Syncing docker-compose.yaml..."
+git pull origin main
+
+# 2. Stop and clean existing containers
+echo "Removing old containers..."
+docker compose down
+
+# # 3. Pull images (no builds)
+# echo "Pulling latest images..."
+# docker-compose pull
+
+# 4. Start containers
+echo "Starting services..."
+docker compose up -d
+
+# 5. Show status
+echo "✅ Current status:"
+docker compose ps
